98e5f2871a3172e472814a2f565472c636e5d1b7
DevWithEasy <devwitheasy@gmail.com>
2024-04-08 13:15:28 +0600

    code refractiot

--- routes/routes.js
+++ routes/routes.js
@@ -29,22 +29,12 @@ const routes = [
   {
     path: "/api/report",
     handler : reportRouter
-  },
-  {
-    path: "/",
-    handler : (req, res) =>{
-      res.send("Alhamdulillah.Server is ready !")
-    }
   }
 ]
 
 const applyRoutes = (app)=>{
   routes.map(r=>{
-    if(r.path === "/"){
-      app.get(r.path,r.handler)
-    }else{
-      app.use(r.path,r.handler)
-    }
+    app.use(r.path,r.handler)
   })
 }
 
